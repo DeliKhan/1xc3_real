db67f9c821808924d106a5b21034e4183a73565f
DeliKhan <deluckshanmurugamoorthy@gmail.com>
2023-06-21 09:45:05 -0400

    print hello mcmaster from class

---
+++ Hello_new.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// the main function
+int main(void)
+{
+	/* calling " printf " function
+	the " definition " is included in " stdio " library */
+	printf("Hello, McMaster!\n");
+}
+
