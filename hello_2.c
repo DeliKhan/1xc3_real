a13c9d82ced37321be79af5274f163dba12cb6e4
DeliKhan <deluckshanmurugamoorthy@gmail.com>
2023-06-20 22:41:31 -0400

    initial set

---
+++ hello_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, ");
+    printf("World!\n") // This line is missing a semicolon!
+}
+
+/* this is a multiline comment
+note the following mistakes:
+    - the "hello" doesn't get printed despite being before the error?
+        - Its cause its compiled, so no
+*/
